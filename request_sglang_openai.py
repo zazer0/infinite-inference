18adadcfae1fe85cdc1e5b896b00299c82dc3a90
zazer0 <zazer.unsw@gmail.com>
2025-05-28 19:34:05 +1000

    feat(sglang-both): use PORT from single cfg file

--- request_sglang_openai.py
+++ request_sglang_openai.py
@@ -1,8 +1,10 @@
 import openai
 import sys
+import PORT from ./config.py
 
 
-PORT=sys.argv[1]
+#PORT=sys.argv[1]
+#PORT=31111
 
 
 client = openai.Client(base_url=f"http://127.0.0.1:{PORT}/v1", api_key="None")
--- run_sglang_openai.py
+++ run_sglang_openai.py
@@ -7,7 +7,9 @@ from sglang.utils import launch_server_cmd
 
 from sglang.utils import wait_for_server, print_highlight, terminate_process
 
-PORT=31111
+import PORT from ./config.py
+
+#PORT=31111
 
 server_process, port = launch_server_cmd(
     "python3 -m sglang.launch_server --model-path IlyaGusev/gemma-2-2b-it-abliterated --context-length 1024 --host 0.0.0.0 --mem-fraction-static 0.3 --attention-backend flashinfer", port=PORT
