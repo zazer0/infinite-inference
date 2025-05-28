ac6611e04d08d908c1cdcdc2257ed8650e55be26
zazer0 <zazer.unsw@gmail.com>
2025-05-28 19:32:56 +1000

    feat(run_sglang): WORKING static port set!

--- run_sglang_openai.py
+++ run_sglang_openai.py
@@ -7,10 +7,11 @@ from sglang.utils import launch_server_cmd
 
 from sglang.utils import wait_for_server, print_highlight, terminate_process
 
+PORT=31111
 
 server_process, port = launch_server_cmd(
-    "python3 -m sglang.launch_server --model-path IlyaGusev/gemma-2-2b-it-abliterated --context-length 1024 --host 0.0.0.0 --mem-fraction-static 0.3 --attention-backend flashinfer"
+    "python3 -m sglang.launch_server --model-path IlyaGusev/gemma-2-2b-it-abliterated --context-length 1024 --host 0.0.0.0 --mem-fraction-static 0.3 --attention-backend flashinfer", port=PORT
 )
 
-wait_for_server(f"http://localhost:{port}")
-print(f"Server started on http://localhost:{port}")
+wait_for_server(f"http://localhost:{PORT}")
+print(f"Server started on http://localhost:{PORT}")
